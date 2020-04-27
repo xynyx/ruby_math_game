a10217088ef2db4da532c568225d6d8e9e02ab32
xynyx <matt.taylr22@gmail.com>
2020-04-27 15:08:38 +0000

    Planning file

---
+++ math_game.rb
@@ -0,0 +1,20 @@
+# 3 Lives
+# Output scores at end of every turn
+# When someone dies, game announces who won and the other players score
+
+# Pick number between 1-20
+
+#Classes = Player1, Player2, Questions, Game score
+
+# PLayer 1 methods
+# Life total
+# Where they pick 2 numbers
+
+
+# Questions
+# Where questions are generated
+# Takes in answer and compares it to correct answer
+
+# Game Score
+# Manages current player
+# Manages score
